554eaef1ec9b1ff3271193432c41a58ad701d3f3
Bohdan Bryk <bohdan.bryk@ucu.edu.ua>
2021-03-04 22:28:42 +0200

    add documentation

--- menu.py
+++ menu.py
@@ -1,9 +1,23 @@
+import doctest
 import sys
 from notebook import Notebook, Note
 
 
 class Menu:
+    """
+    Menu class. Contains __init__ , display_menu, run, show_notes,
+    search_notes, add_note, modify_note, quit methods
+    """
+
     def __init__(self):
+        """
+
+        main method to create object. It is called when
+        the new object is being created
+
+        >>> menu = Menu()
+
+        """
         self.notebook = Notebook()
         self.choices = {
             "1": self.show_notes,
@@ -15,6 +29,21 @@ class Menu:
         }
 
     def display_menu(self):
+        """
+        dispays the menu
+
+        >>> menu = Menu()
+        >>> menu.display_menu()
+        <BLANKLINE>
+        <BLANKLINE>
+                Notebook Menu
+        <BLANKLINE>
+                1. Show all Notes
+                2. Search Notes
+                3. Add Note
+                4. Modify Note
+                5. Quit
+        """
         print("""
         
         Notebook Menu
@@ -26,44 +55,62 @@ class Menu:
         5. Quit""")
 
     def run(self):
-        '''Display the menu and respond to choices.'''   
+        '''Display the menu and respond to choices.'''
         while True:
-            self.display_menu()      
-            choice = input("Enter an option: ")       
+            self.display_menu()
+            choice = input("Enter an option: ")
             action = self.choices.get(choice)
-            if action:        
-                action()    
-            else:       
+            if action:
+                action()
+            else:
                 print("{0} is not a valid choice".format(choice))
 
     def show_notes(self, notes=None):
+        """
+        displays the notes
+        """
         if not notes:
             notes = self.notebook.notes
         for note in notes:
             print(f"{note.id}: {note.tags},\n{note.memo}")
-        
+
     def search_notes(self):
+        """
+        searches fot the notes using the keyword
+
+        """
         key_word = input("Search for:")
         notes = self.notebook.search(key_word)
         self.show_notes(notes)
 
     def add_note(self):
+        """
+        adds a note
+        """
         memo = input("Enter a memo:")
         self.notebook.new_note(memo)
         print("Your note has been added")
 
     def modify_note(self):
+        """
+        modifies the note
+        """
         id = input("enter a note id:")
         memo = input("Enter a memo:")
         tags = input("Enter tags:")
         if memo:
             self.notebook.modify_memo(id, memo)
         if tags:
-            self.notebook.modify_tags(id,tags)
+            self.notebook.modify_tags(id, tags)
 
     def quit(self):
+        """
+        ends the program
+        """
         print("Thank you")
         sys.exit(0)
 
-if __name__=="__main__":
-    Menu().run()
+
+# if __name__=="__main__":
+#     Menu().run()
+doctest.testmod()
--- notebook.py
+++ notebook.py
@@ -4,7 +4,17 @@ last_id = 0
 
 
 class Note:
+    """
+    Note class. Contains __init__ and match methods
+    """
     def __init__(self, memo, tags=''):
+        """
+        main method to create object. It is called when
+        the new object is being created
+        >>> note = Note('hello')
+        >>> print(note.memo)
+        hello
+        """
         self.memo = memo
         self.tags = tags
         self.creation_date = datetime.date.today()
@@ -13,37 +23,97 @@ class Note:
         self.id = last_id
 
     def match(self, key_word):
+        """
+        method to check whether the keyword mathes the memo of note
+        >>> note = Note('hello')
+        >>> note.match('hell')
+        True
+        """
         isExisting = key_word in self.memo or key_word in self.tags
         return isExisting
 
 
 class Notebook:
+    """
+    Notebook class. Contains __init__ , new_note , find__note, modify_memo, modify_tags, search methods
+    """
     def __init__(self):
+        """
+        main method to create object. It is called when
+        the new object is being created
+        
+        >>> notebook = Notebook()
+        >>> print(notebook.notes)
+        []
+        """
         self.notes = []
 
     def new_note(self, memo, tags=''):
+        """
+        adds a new note
+
+        >>> notebook = Notebook()
+        >>> notebook.new_note('yes')
+        >>> print(notebook.notes[0].memo)
+        yes
+        """
+
         self.notes.append(Note(memo, tags))
 
-    def __find__note(self, note_id):
+    def find__note(self, note_id):
+        """
+        finds the specific note
+
+        >>> notebook = Notebook()
+        >>> notebook.new_note('yes')
+        >>> notebook.find__note(1)
+        
+        """
         for note in self.notes:
             if str(note.id) == str(note_id):
                 return note
         return None
 
     def modify_memo(self, note_id, memo):
+        """
+        modifies the memo of note
 
-        note = self.__find__note(note_id)
+        >>> notebook = Notebook()
+        >>> notebook.new_note('yes')
+        >>> notebook.modify_memo(1,'qwerty')
+        False
+        """
+
+        note = self.find__note(note_id)
         if note:
             note.memo = memo
             return True
         return False
 
     def modify_tags(self, note_id, tags):
-        note = self.__find__note(note_id)
+        """
+        modifies the tags of specific note
+
+        >>> notebook = Notebook()
+        >>> notebook.new_note('yes')
+        >>> notebook.modify_tags(1,'tag1')
+        False
+        """
+        note = self.find__note(note_id)
         if note:
             note.tags = tags
             return True
         return False
 
     def search(self, key_word):
+        """
+        searches the note by keyword
+
+
+        >>> notebook = Notebook()
+        >>> notebook.new_note('yes')
+        """
         return (note for note in self.notes if note.match(key_word))
+
+import doctest
+doctest.testmod()
